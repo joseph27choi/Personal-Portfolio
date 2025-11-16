76c193e4254cc4412c313ab54aa52c66a6bacc1d
joseph27choi <choi.joseph0302@gmail.com>
2025-11-15 20:25:05 -0500

    optimized

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Experience from './components/Experience'
-import Contact from './components/Contact'
+// import Contact from './components/Contact'
 import './App.css'
 
 function App() {
